b984b0b9088043d0ef510823abeca8b98ccea04f
Yash Saran <ysaran@cogitate.us>
2023-06-16 12:00:01 +0530

    versel changes

--- Models/Ticket.js
+++ Models/Ticket.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 
 const TicketSchema = mongoose.Schema({
-    seat_number: { type: Number, min: 1, max: 40, required: true },
+    seat_number: { type: Number, min: 1, max: 40, required: true,unique:true },
     is_booked: { type: Boolean, default: true },
     date: { type: Date, default: Date.now() },
     passenger: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
--- routes/operations.js
+++ routes/operations.js
@@ -1,76 +1,76 @@
-import express from 'express';
-import Ticket from '../Models/Ticket.js';
-import User from '../Models/User.js'
+import express from "express";
+import Ticket from "../Models/Ticket.js";
+import User from "../Models/User.js";
 
-const route=express.Router();
+const route = express.Router();
 
-route.post('/ticket',async (req,res)=>{
-    try {
-        const ticket=new Ticket({seat_number:req.body.seat_number});
-        const user=new User(req.body.passenger);
+route.post("/ticket", async (req, res) => {
+  try {
+    const ticket = new Ticket({ seat_number: req.body.seat_number });
+    const user = new User(req.body.passenger);
 
-        user.save().then(data=>{
-            
-            if(data){
-                ticket.passenger=data._id;
-                ticket.save().then(data=>{
-                    res.status(200).json(data);
-                });
-                        
-        }
+    user.save().then((data) => {
+      if (data) {
+        ticket.passenger = data._id;
+        ticket.save().then((data) => {
+          res.status(200).json(data);
         });
-        
-    } catch (error) {
-        res.status(400).json(error);
-    }
+      }
+    });
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
+route.get("/ticket/all", async (req, res) => {
+  Ticket.find({}).then((data) => {
+    if (data) res.status(200).json(data);
+    else res.status(200).json({ result: "No data found" });
+  });
+});
+
+
+
+route.get("/ticket/status/:ticketid", async (req, res) => {
+  Ticket.find({ _id: req.params.ticketid }).then((data) => {
+    if (data.length) res.status(200).json(data);
+    else res.status(200).json({ result: "No data found" });
+  });
+});
+
+route.get("/ticket/open", async (req, res) => {
+  Ticket.find({ is_booked: false }).then((data) => {
+    if (data.length) res.status(200).json(data);
+    else res.status(200).json({ result: "No data found" });
+  });
+});
+
+route.get("/ticket/closed", async (req, res) => {
+  Ticket.find({ is_booked: true }).then((data) => {
+    if (data.length) res.status(200).json(data);
+    else res.status(200).json({ result: "No data found" });
+  });
+});
+
+route.get("/ticket/user/:ticketid", async (req, res) => {
+  Ticket.find({ _id: req.params.ticketid }).then((data) => {
+    if (data.length) {
+      User.find({ _id: data[0].passenger }).then((data) => {
+        res.status(200).json(data);
+      });
+    } else res.status(200).json({ result: "No Ticket Found" });
+  });
+});
+
+route.get("/user/all", async (req, res) => {
+    User.find({}).then((data) => {
+      if (data) res.status(200).json(data);
+      else res.status(200).json({ result: "No data found" });
+    });
+  });
+
+  route.put("/ticket/update",async(req,res)=>{
     
-
-
-})
-
-route.get('/ticket/all',async (req,res)=>{
-    Ticket.find({}).then(data=>{
-        if(data)
-         res.status(200).json(data);
-        else
-         res.status(200).json({'result':'No data found'});
-    });
-})
-
-route.get('/user/all',async (req,res)=>{
-    User.find({}).then(data=>{
-        if(data)
-         res.status(200).json(data);
-        else
-         res.status(200).json({'result':'No data found'});
-    });
-})
-
-route.get('/ticket/status/:ticketid',async (req,res)=>{
-    Ticket.find({_id:req.params.ticketid}).then(data=>{
-        if(data.length)
-         res.status(200).json(data);
-        else
-         res.status(200).json({'result':'No data found'});
-    });
-})
-
-route.get('/ticket/open',async (req,res)=>{
-    Ticket.find({is_booked:false}).then(data=>{
-        if(data.length)
-         res.status(200).json(data);
-        else
-         res.status(200).json({'result':'No data found'});
-    });
-})
-
-route.get('/ticket/closed',async (req,res)=>{
-    Ticket.find({is_booked:true}).then(data=>{
-        if(data.length)
-         res.status(200).json(data);
-        else
-         res.status(200).json({'result':'No data found'});
-    });
-})
+  })
 
 export default route;
