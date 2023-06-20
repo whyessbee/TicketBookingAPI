98544115ab83d06b0bfe30e591edccc84f6bba65
Yash Saran <ysaran@cogitate.us>
2023-06-20 12:42:55 +0530

    verify jwt and some changes in error catch for EPs

--- routes/operations.js
+++ routes/operations.js
@@ -5,62 +5,80 @@ import verifyToken from "../lib/verify.controller.js"
 
 const route = express.Router();
 
-route.post("/ticket", async (req, res) => {
+route.post("/ticket", verifyToken,async (req, res,next) => {
   try {
+    if (!req.id) {
+        res.status(403)
+          .send({
+            message: "Invalid JWT token"
+          });
+      }
     const ticket = new Ticket({ seat_number: req.body.seat_number });
-    const user = new User(req.body.passenger);
+    //const user = new User(req.body.passenger);
 
-    user.save().then((data) => {
-      if (data) {
-        ticket.passenger = data._id;
+   
+      
+        ticket.passenger = req.id;
         ticket.save().then((data) => {
           res.status(200).json(data);
-        });
-      }
-    });
-  } catch (error) {
+        }).catch((err)=>{
+            next(err);
+        })
+    }
+ 
+   catch (error) {
     res.status(400).json(error);
   }
 });
 
-route.get("/ticket/all", async (req, res) => {
+route.get("/ticket/all", async (req, res,next) => {
   Ticket.find({}).then((data) => {
     if (data) res.status(200).json(data);
     else res.status(200).json({ result: "No data found" });
+  }).catch((err)=>{
+    next(err);
   });
 });
 
 
 
-route.get("/ticket/status/:ticketid", async (req, res) => {
+route.get("/ticket/status/:ticketid", async (req, res,next) => {
   Ticket.find({ _id: req.params.ticketid }).then((data) => {
     if (data.length) res.status(200).json(data);
     else res.status(200).json({ result: "No data found" });
-  });
+  }).catch((err)=>{
+    next(err)});
 });
 
-route.get("/ticket/open", async (req, res) => {
+route.get("/ticket/open", async (req, res,next) => {
   Ticket.find({ is_booked: false }).then((data) => {
     if (data.length) res.status(200).json(data);
     else res.status(200).json({ result: "No data found" });
-  });
+  }).catch((err)=>{
+    next(err)});;
 });
 
-route.get("/ticket/closed", async (req, res) => {
+route.get("/ticket/closed", async (req, res,next) => {
   Ticket.find({ is_booked: true }).then((data) => {
     if (data.length) res.status(200).json(data);
     else res.status(200).json({ result: "No data found" });
-  });
+  }).catch((err)=>{
+    next(err)});;
 });
 
 route.get("/ticket/user/:ticketid", async (req, res) => {
   Ticket.find({ _id: req.params.ticketid }).then((data) => {
     if (data.length) {
       User.find({ _id: data[0].passenger }).then((data) => {
+        if(data.length)
         res.status(200).json(data);
-      });
-    } else res.status(200).json({ result: "No Ticket Found" });
-  });
+        else
+        res.status(200).json({'Result':'No User Found For This Ticket Id: '+req.params.ticketid});
+      }).catch((err)=>{
+        next(err)});
+    } else res.status(200).json({ result: "No Ticket Found With Id: "+req.params.ticketid });
+  }).catch((err)=>{
+    next(err)});
 });
 
 // route.put("ticket/yash",async(req,res)=>{
@@ -122,7 +140,7 @@ route.get("/user/all", async (req, res) => {
     
   })
 
-  route.put("/ticket/update/:ticketid",async(req,res,next)=>{
+  route.put("/ticket/update/:ticketid",verifyToken,async(req,res,next)=>{
     try {
         
         Ticket.findById(req.params.ticketid).then(async (ticketData)=>{
@@ -134,7 +152,7 @@ route.get("/user/all", async (req, res) => {
                     const savedUser=await user.save(req.body.passenger)
                     .catch(err=>{
                         next(err);
-                        //res.status(500).json(err.message)
+                        
                     })
                     userId=savedUser._id;
                 
@@ -142,7 +160,7 @@ route.get("/user/all", async (req, res) => {
                         ticketData.passenger=userId;
                         let savedTicket=await ticketData.save().catch(err=>{
                             next(err);
-                            //res.send(500).json(err.message);
+                           
                         });
                         res.status(200).json(savedTicket);
                     }
